81cf022f42fff88ff7de0688a122f1cfec707b2a
RTcY <52824468+LordPiers@users.noreply.github.com>
2020-02-26 13:56:19 +0300

    Proje dosyası ekle.

---
+++ PostgreOgrenci/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PostgreOgrenci.Models;
+
+namespace PostgreOgrenci.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly PostgresContext _ctxpost;
+
+        public ValuesController(PostgresContext ctxpost )
+        {
+            _ctxpost = ctxpost;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = _ctxpost.ogrenci.OrderBy(c => c.Id);
+            return Ok(data);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PostgreOgrenci/Models/PostgresContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostgreOgrenci.Models
+{
+    public class PostgresContext : DbContext
+    {
+        public PostgresContext()
+        {
+
+        }
+        public PostgresContext(DbContextOptions<PostgresContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ogrenci> ogrenci { get; set; }
+    }
+}
