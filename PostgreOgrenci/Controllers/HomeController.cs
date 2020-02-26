7dd105aa383c94efb08a02df2367533a752041b2
Ogun Bilgili <ogunbilgili@gmail.com>
2020-02-26 14:44:49 +0300

    v1.1

---
+++ PostgreOgrenci/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PostgreOgrenci.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        public ActionResult RTcY()
+        {
+            return View();
+        }
+    }
+}
