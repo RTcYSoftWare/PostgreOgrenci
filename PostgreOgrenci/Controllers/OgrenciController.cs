0596f7cebc8d50fc441ec36660ab4953f34a44d6
RTcY <52824468+LordPiers@users.noreply.github.com>
2020-03-08 22:29:13 +0300

    v2.7

---
+++ PostgreOgrenci/Controllers/OgrenciController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PostgreOgrenci.Controllers
+{
+    [Route("[controller]")]
+    public class OgrenciController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("anasayfa")]
+        public ActionResult Anasayfa()
+        {
+            return View();
+        }
+    }
+}
